66fc15dc248f11fd2532b47617950fced2a44d14
Rmin-cmd <79217482+Rmin-cmd@users.noreply.github.com>
2022-07-31 10:59:50 +0430

    Update train.py

--- segmentation_models_pytorch/utils/train.py
+++ segmentation_models_pytorch/utils/train.py
@@ -5,10 +5,13 @@ from .meter import AverageValueMeter
 
 
 class Epoch:
-    def __init__(self, model, loss, metrics, stage_name, device="cpu", verbose=True):
+
+    def __init__(self, model, loss, metrics, individual_metrics, labels, stage_name, device='cpu', verbose=True):
         self.model = model
         self.loss = loss
         self.metrics = metrics
+        self.individual_metrics = individual_metrics
+        self.labels = labels
         self.stage_name = stage_name
         self.verbose = verbose
         self.device = device
@@ -20,10 +23,12 @@ class Epoch:
         self.loss.to(self.device)
         for metric in self.metrics:
             metric.to(self.device)
+        for individual_metric in self.individual_metrics:
+            individual_metric.to(self.device)
 
     def _format_logs(self, logs):
-        str_logs = ["{} - {:.4}".format(k, v) for k, v in logs.items()]
-        s = ", ".join(str_logs)
+        str_logs = ['{} - {:.4}'.format(k, v) for k, v in logs.items()]
+        s = ', '.join(str_logs)
         return s
 
     def batch_update(self, x, y):
@@ -37,15 +42,16 @@ class Epoch:
         self.on_epoch_start()
 
         logs = {}
+        individual_logs = {}
         loss_meter = AverageValueMeter()
         metrics_meters = {metric.__name__: AverageValueMeter() for metric in self.metrics}
 
-        with tqdm(
-            dataloader,
-            desc=self.stage_name,
-            file=sys.stdout,
-            disable=not (self.verbose),
-        ) as iterator:
+        label_meters = {}
+        for metric in self.individual_metrics:
+            for label in self.labels:
+                label_meters[label + '_' + metric.__name__] = AverageValueMeter()
+
+        with tqdm(dataloader, desc=self.stage_name, file=sys.stdout, disable=not (self.verbose)) as iterator:
             for x, y in iterator:
                 x, y = x.to(self.device), y.to(self.device)
                 loss, y_pred = self.batch_update(x, y)
@@ -53,30 +59,45 @@ class Epoch:
                 # update loss logs
                 loss_value = loss.cpu().detach().numpy()
                 loss_meter.add(loss_value)
-                loss_logs = {self.loss.__name__: loss_meter.mean}
+                loss_logs = {self.loss.__name__.capitalize(): loss_meter.mean}
                 logs.update(loss_logs)
 
                 # update metrics logs
                 for metric_fn in self.metrics:
                     metric_value = metric_fn(y_pred, y).cpu().detach().numpy()
                     metrics_meters[metric_fn.__name__].add(metric_value)
-                metrics_logs = {k: v.mean for k, v in metrics_meters.items()}
+
+                for metric_fn in self.individual_metrics:
+                    label_values = metric_fn(y_pred, y)
+                    for label, label_value in zip(self.labels, label_values):
+                        lv = label_value.cpu().detach().numpy()
+                        label_meters[label+'_'+metric_fn.__name__].add(lv)
+
+                metrics_logs = {k.capitalize(): v.mean for k, v in metrics_meters.items()}
+                label_logs = {k.capitalize(): v.mean for k, v in label_meters.items()}
+
                 logs.update(metrics_logs)
+                individual_logs.update(loss_logs)
+                individual_logs.update(metrics_logs)
+                individual_logs.update(label_logs)
 
                 if self.verbose:
                     s = self._format_logs(logs)
                     iterator.set_postfix_str(s)
 
-        return logs
+        return logs, individual_logs
 
 
 class TrainEpoch(Epoch):
-    def __init__(self, model, loss, metrics, optimizer, device="cpu", verbose=True):
+
+    def __init__(self, model, loss, metrics, individual_metrics, labels, optimizer, device='cpu', verbose=True):
         super().__init__(
             model=model,
             loss=loss,
             metrics=metrics,
-            stage_name="train",
+            individual_metrics=individual_metrics,
+            labels=labels,
+            stage_name='train',
             device=device,
             verbose=verbose,
         )
@@ -95,12 +116,15 @@ class TrainEpoch(Epoch):
 
 
 class ValidEpoch(Epoch):
-    def __init__(self, model, loss, metrics, device="cpu", verbose=True):
+
+    def __init__(self, model, loss, metrics, individual_metrics, labels, device='cpu', verbose=True):
         super().__init__(
             model=model,
             loss=loss,
             metrics=metrics,
-            stage_name="valid",
+            individual_metrics=individual_metrics,
+            labels=labels,
+            stage_name='valid',
             device=device,
             verbose=verbose,
         )
